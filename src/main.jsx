01052fbbbfbdddb5805f2585515167eac110d771
Kritika745 <yadavkritika745@gmail.com>
2024-01-12 19:35:53 +0530

    callforpaper

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import './App.css'
 import App from './App.jsx'
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
 import Layout from './Layout.jsx'
