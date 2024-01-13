1b3d50ff0e80364dd584caed6a3d3aa54daca61e
Harsh Yadav <hy945196@gmail.com>
2024-01-13 12:43:06 +0530

    Time Fixed

---
+++ src/Components/Timer.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+
+const Timer = () => {
+
+  const [days, setDays] = useState(0);
+  const [hours, setHours] = useState(0);
+  const [mins, setMinutes] = useState(0);
+  const [secs, setSeconds] = useState(0);
+  const deadline = "February, 23, 2024";
+  const getTime = () => {
+     const time = Date.parse(deadline) - Date.now();
+     setDays(Math.floor(time / (1000 * 60 * 60 * 24)));
+     setHours(Math.floor((time / (1000 * 60 * 60)) % 24));
+     setMinutes(Math.floor((time / 1000 / 60) % 60));
+     setSeconds(Math.floor((time / 1000) % 60));
+ };
+
+ useEffect (()=>{
+    const interval = setInterval(()=>getTime(deadline), 1000)
+    return ()=> clearInterval(interval)
+    }, [])
+  return (
+    <div>
+        <div className="flex sm:mt-0 mt-10 sm:gap-20 gap-7 w-full items-center justify-center">
+            <div className='sm:w-16'>
+              <h1 className='sm:text-[4vw] text-[8vw] font-sans font-semibold'>{days<10 ? "0"+days: days}</h1>
+              <h3 className='text-center sm:text-xl font-sans font-semibold'>Days</h3>
+            </div>
+            <div className='sm:w-16'>
+              <h1 className='sm:text-[4vw] text-[8vw] font-sans font-semibold'>{hours <10 ? "0"+hours: hours}</h1>
+              <h3 className='text-center sm:text-xl font-sans font-semibold'>Hours</h3>
+            </div>
+            <div className='sm:w-16'>
+              <h1 className='sm:text-[4vw] text-[8vw] font-sans font-semibold'>{mins<10 ? "0"+mins: mins}</h1>
+              <h3 className='text-center sm:text-xl font-sans font-semibold'>Minutes</h3>
+            </div>
+            <div className='sm:w-16'>
+              <h1 className='sm:text-[4vw] text-[8vw] font-sans font-semibold'>{secs<10 ? "0"+secs: secs}</h1>
+              <h3 className='text-center sm:text-xl font-sans font-semibold'>Seconds</h3>
+            </div>
+          </div>
+    </div>
+  )
+}
+
+export default Timer
