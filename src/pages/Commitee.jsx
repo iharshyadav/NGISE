3a9f8bcf7f80d3016dfcd611b44447b58d53add2
traptijindal <traptijindal5678@gmail.com>
2024-06-08 19:12:48 +0530

    data added

--- src/pages/Commitee.jsx
+++ src/pages/Commitee.jsx
@@ -36,6 +36,8 @@ const Commitee = () => {
       </div>
       <div className='w-full text-base lg:text-lg  sm:w-4/5 ml-4 '>
         <p>Dr. Rahul Sharma, HOD-IT,  AKGEC GHAZIABAD, INDIA</p>
+        <p>Dr. Anupama Sharma Ajay Kumar Garg Engineering College, Ghaziabad, India
+</p>
       </div>
     </div>
   
@@ -74,6 +76,8 @@ const Commitee = () => {
       <div className='w-full text-base lg:text-lg  sm:w-4/5 ml-4'>
         <p>Dr. Anu Chaudhary, AKGEC GHAZIABAD, INDIA</p>
         <p>Dr. Rajesh Prasad, AKGEC GHAZIABAD, INDIA</p>
+        <p>Dr. Markus Bertl Tallinn University, Estonia </p>
+        <p>Dr. Rahul Sharma Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
       </div>
     </div>
 
@@ -97,10 +101,33 @@ const Commitee = () => {
 
     <div className='flex flex-col sm:flex-row my-4'>
       <div className='w-full sm:w-1/5 mx-2 sm:mx-4 mb-2 sm:mb-4 p-2 linearGradient rounded-md shadow-lg'>
+        <p className='font-bold text-base lg:text-lg  text-white'>Treasurer(s)</p>
+      </div>
+      <div className='w-full text-base lg:text-lg sm:w-4/5 ml-4'>
+        <p>Dr. Ruchi Gupta Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+        <p>Dr. Rashmi Sharma Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+      </div>
+    </div>
+
+    <div className='flex flex-col sm:flex-row my-4'>
+      <div className='w-full sm:w-1/5 mx-2 sm:mx-4 mb-2 sm:mb-4 p-2 linearGradient rounded-md shadow-lg'>
         <p className='font-bold text-base lg:text-lg  text-white'>Technical Program Chair</p>
       </div>
       <div className='w-full text-base lg:text-lg  sm:w-4/5 ml-4'>
         <p>Dr. Aditya Pratap Singh, AKGEC GHAZIABAD, INDIA</p>
+        <p>Dr. Sunil Kumar Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+        <p>Dr. Nandita Goyal Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+      </div>
+    </div>
+
+    <div className='flex flex-col sm:flex-row my-4'>
+      <div className='w-full sm:w-1/5 mx-2 sm:mx-4 mb-2 sm:mb-4 p-2 linearGradient rounded-md shadow-lg'>
+        <p className='font-bold text-base lg:text-lg  text-white'>Publicity Chair</p>
+      </div>
+      <div className='w-full text-base lg:text-lg  sm:w-4/5 ml-4'>
+        <p>Dr. Shivani Agrawal Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+        <p>Dr. Avdhesh Gupta Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+        
       </div>
     </div>
 
@@ -116,6 +143,23 @@ const Commitee = () => {
         <p>Dr. Colin Atkinson University of Mannheim, Germany</p>
         <p>Dr. Veena Bharti  AKGEC GHAZIABAD, INDIA</p>
         <p>Dr. Nandita Goyal  AKGEC GHAZIABAD, INDIA</p>
+        <p>Dr. Prayag Tiwari Halmstad University, Sweden</p>
+        <p>Dr. Shobha Tyagi Manav Rachna International Institute of Research and studies, India</p>
+        <p>Dr. Pushpa Singh GL Bajaj Institute of Technology & Management, Greater Noida, India</p>
+        <p>Dr. Umesh Gupta Bennett University, India</p>
+        <p>Dr. Amrita Jyoti ABES Engineering College, India</p>
+        <p>Dr. Anju Mishra Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+        <p>Dr. Nitin Sharma Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+        <p>Dr. Shailza Kanwar Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+        <p>Ms. Tahira Mazumder Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+        <p>Mr. Sumit Sharma Ajay Kumar Garg Engineering College,Ghaziabad, India</p>
+        <p>Mr. Birendra Tiwari Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+        <p>Mr. Pancham Singh Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+        <p>Mr. Santosh Verma Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+        <p>Mr. Pradeep Tripathi Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+        <p>Mr. Achintya Pandey Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+        <p>Mr. Anil Singh Chauhan Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
+        <p>Mr. Sanjay Sonkar Ajay Kumar Garg Engineering College, Ghaziabad, India</p>
       </div>
     </div>
 
