dda8ed634ff904448e248121c1cf8155944cb0f6
traptijindal <traptijindal5678@gmail.com>
2024-08-24 09:38:57 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -9,10 +9,11 @@ import Contact from './pages/Contact';
 import About from './pages/About';
 import Commitee from './pages/Commitee';
 import Accomodation from './pages/Accomodation';
-import Registration from './pages/Registration';
 import CallForPaper from './pages/CallForPaper.jsx'
 import Acceptedpaper from './pages/Acceptedpaper.jsx'
 import PaperSubmission from './pages/PaperSubmission.jsx'
+import RegistrationFee from './pages/RegistrationFee.jsx'
+import FeeSubmission from './pages/FeeSubmission.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -22,7 +23,8 @@ const router = createBrowserRouter(
         <Route path="/about" element={<About />} />
         <Route path="/commitee" element={<Commitee />} />
         <Route path="/accomodation" element={<Accomodation />} />
-        <Route path="/registration" element={<Registration />} />
+        <Route path="/registration" element={<RegistrationFee />} />
+        <Route path="/fee" element={<FeeSubmission />} />
         <Route path="/papersubmission" element={<PaperSubmission />} />
         <Route path="/callforpaper" element={<CallForPaper />} />
         <Route path='/speakers' element ={<Acceptedpaper/>}/>
