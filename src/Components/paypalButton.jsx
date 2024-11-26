791334e330c2bac275d933087fb0466ad741792c
Harsh Yadav <hy945196@gmail.com>
2024-11-26 08:07:20 +0530

    registration form

--- src/Components/paypalButton.jsx
+++ src/Components/paypalButton.jsx
@@ -30,7 +30,11 @@ const PayPalButton = () => {
     };
   }, []);
 
-  return <div id="paypal-container-Y88H3GLYEMY98"></div>;
+  return (
+    <div className="flex justify-center items-center min-h-screen">
+        <div id="paypal-container-Y88H3GLYEMY98" className="p-4 border border-gray-300 rounded-md bg-white shadow-md"></div>
+    </div>
+  )
 };
 
 export default PayPalButton;
--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
 import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
-import { Outlet } from 'react-router-dom'
+import { Outlet, useLocation } from 'react-router-dom'
 
 function Layout() {
+
+  const location = useLocation()
+  console.log(location)
   return (
     <>
+    {
+      location === "/payment" ?
+      <>
       <Navbar classname={`z-50 top-0`} />
       <Outlet/>
       <Footer/>
+      </>
+      : <Outlet/>
+    }
     </>
   )
 }
